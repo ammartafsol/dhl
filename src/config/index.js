6d2875b5eeda4ce173fa82d3bf513b44ccf41021
ammartafsol <ammar.js@tafsol.com>
2025-07-23 14:52:23 +0500

    updated

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 export const config = {
-  apiBaseURL: "https://gj2h0qqn-3050.inc1.devtunnels.ms", // faiz
+  // apiBaseURL: "https://gj2h0qqn-3050.inc1.devtunnels.ms", // faiz
   // apiBaseURL: "https://gj2h0qqn-3050.inc1.devtunnels.ms", // asad
   // apiBaseURL: "https://gj2h0qqn-3050.inc1.devtunnels.ms", // FAIZ
 
-  // apiBaseURL: "https://marks-backend-b11-bed55d892708.herokuapp.com", //live new
+  apiBaseURL: "https://marks-backend-b11-bed55d892708.herokuapp.com", //live new
   // apiBaseURL: "https://gj2h0qqn-3031.inc1.devtunnels.ms",
   s3BucketURL: "https://wajba-bucket.s3.us-east-2.amazonaws.com", // LIVEs
   stripePublicKey: "",
